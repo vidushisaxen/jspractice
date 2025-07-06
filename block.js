01fefffe505dc5e12e81b92e8d78be278f6b8a9e
Vidushi Saxena <vidushisaxenabdn786@gmail.com>
2025-07-06 21:36:43 +0530

    block scoped , block

---
+++ block.js
@@ -0,0 +1,63 @@
+ // let and const are block scoped - accessible inside their block only , var is global scoped
+ {
+    // this ia a block or compound statements - used to combine multiple js statements into one 
+    // var a =20;
+    // console.log(a);
+    // wecan use this block as a whole where JS is expecting only one statement
+ }
+
+//  if(true) console.log("Hello")
+
+//     if(true) {
+//         var a =20;
+//     console.log(a);
+//     }
+
+
+//Shadowing
+
+// var a1=100; // abc
+// let b=100;
+// const c = 100;
+// // console.log(a1);
+// // console.log(b);
+// // console.log(c);
+// {
+//         var a1= "abc"; //shadows a1 : bcz first it looks in local scope and also modified the value
+//         let b  = "pqr";
+//         const c = "xyz";
+// console.log(a1); //abc
+// console.log(b); //pqr
+// console.log(c); //xyz
+//     }
+// console.log(a1); //abc
+// console.log(b);
+// console.log(c);
+
+// we can shadow a let using a let  , var can be shadowed with let , const will shadow with const only 
+
+
+// let b =100; //Illegal Shadowing
+// {
+//     var b =20;
+// }
+
+
+// var b =100;
+// {
+//     let b =20;
+//     console.log(b);
+    
+// }
+// console.log(b);
+
+var b =100;
+{
+    const b =20;
+    console.log(b);//20
+}
+console.log(b); //100
+
+
+
+
---
+++ let-const.js
@@ -0,0 +1,46 @@
+// let and const declarations are hoisted ...
+//they are in the Temporal Dead Zone(TDZ) - the phase from hoisting till it gets some values
+//whenever we try to accessa a variable in TDZ it gives a reference error 
+// when the variables are in TDZ , we cannot access them, they are accessible once the values has been initialized
+// We can reduce TDZ by moving our initializations at the top of their scope
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var a =10;
+
+// let b=100;
+
+// const c =1000;
+
+// var a = 30;
+
+// console.log(a);
+
+
+// let c; //declaration
+//  c="abc" //initializing value
+
+//  console.log(c);
+ 
+
+// let b = 100;
+
+// let b  = 200; //Syntax error 
+
+// console.log(b);
+  
+
+// const c="abc";
+// const c="pqr"
+
+// const x =100; //type error
+// // x=10;
+// console.log(x);
+
+// const y = 10;
+
+
+
+
